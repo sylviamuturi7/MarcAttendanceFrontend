42a74abf83585854fd1c42965baffde8f7967d9c
abdiaziz <abdiazizabdullahi40@gmail.com>
2026-05-15 10:20:44 +0300

    MA-58 Creation of context

---
+++ react/src/context/ThemeContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) setTheme(savedTheme);
+  }, []);
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export default ThemeProvider;
