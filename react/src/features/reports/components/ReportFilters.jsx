c84ac1fcee41c38b63092be8a967271a2046d8f3
sylviamuturi7 <sylviamuturi7@gmail.com>
2026-05-15 23:22:16 +0300

    MA-64 adding loading states in Reportfilters

--- react/src/features/reports/components/ReportFilters.jsx
+++ react/src/features/reports/components/ReportFilters.jsx
@@ -1,14 +1,16 @@
-function ReportFilters({ date, department, onDateChange, onDepartmentChange, departments }) {
+function ReportFilters({ date, department, onDateChange, onDepartmentChange, departments, loading }) {
+  if (!departments) return null;
+
   return (
     <div className="report-filters">
       <div>
         <label>Date</label>
-        <input type="date" value={date} onChange={(e) => onDateChange(e.target.value)} />
+        <input type="date" value={date} onChange={(e) => onDateChange(e.target.value)} disabled={loading} />
       </div>
 
       <div>
         <label>Department</label>
-        <select value={department} onChange={(e) => onDepartmentChange(e.target.value)}>
+        <select value={department} onChange={(e) => onDepartmentChange(e.target.value)} disabled={loading}>
           <option value="">All Departments</option>
           {departments.map((dept) => (
             <option key={dept} value={dept}>{dept}</option>
