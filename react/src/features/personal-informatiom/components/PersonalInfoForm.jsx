fa4494036ffa1f769bf04d1c9237c2001495d4bb
sylviamuturi7 <sylviamuturi7@gmail.com>
2026-05-16 06:59:51 +0300

    MA-74 PersonalInfoForm Trial 1

---
+++ react/src/features/personal-informatiom/components/PersonalInfoForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function PersonalInfoForm({ admin, onSubmit }) {
+  if (!admin) return <p>No profile information found.</p>;
+
+  const [formData, setFormData] = useState({
+    firstName: admin.firstName,
+    lastName: admin.lastName,
+    email: admin.email,
+  });
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit(formData);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-row">
+        <div>
+          <label>First Name</label>
+          <input name="firstName" defaultValue={admin.firstName} onChange={handleChange} />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input name="lastName" defaultValue={admin.lastName} onChange={handleChange} />
+        </div>
+      </div>
+
+      <label>Email Address</label>
+      <input name="email" defaultValue={admin.email} onChange={handleChange} />
+
+      <button type="submit">Save Changes</button>
+    </form>
+  );
+}
+
+export default PersonalInfoForm;
