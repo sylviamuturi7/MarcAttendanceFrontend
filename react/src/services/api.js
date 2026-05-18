922afd7f93e42b690d4f2e87c8a43888a2ad62d6
sylviamuturi7 <sylviamuturi7@gmail.com>
2026-05-18 16:45:43 +0300

    MA-84 Do services

--- react/src/services/api.js
+++ react/src/services/api.js
@@ -4,4 +4,4 @@ const api = axios.create({
   baseURL: "http://localhost:5174/api",
 });
 
-export default api;
+export default api; 
--- react/src/services/axiosInterceptor.js
+++ react/src/services/axiosInterceptor.js
@@ -5,7 +5,7 @@ api.interceptors.request.use((config) => {
   if (token) {
     config.headers.Authorization = "Bearer " + token;
   }
-  return config;
+  return config; 
 });
 
 api.interceptors.response.use(
